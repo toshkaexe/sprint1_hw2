fec894cac505fba241b7e6c838457d1e0fada9e6
Anton <anton.zeltser@gmail.com>
2023-12-18 21:06:33 +0100

    init commit for the homework2

---
+++ src/routes/post-route.ts
@@ -0,0 +1,77 @@
+import {Router, Request, Response} from 'express';
+import {BlogRepository} from "../repositories/blog-repository";
+import {authMiddleware} from "../middlewares_validation/auth-middlewares";
+import {blogValidation, nameValidation} from "../validators/blog-validator";
+import {PostRepository} from "../repositories/post-repository";
+
+import {BlogBody, Params, RequestWithBody, RequestWithParams} from "../models/common";
+import {randomUUID} from "crypto";
+import {blogRoute} from "./blog-route";
+import {CreatePostModel} from "../models/posts/input";
+import {postValidation} from "../validators/post-validator";
+import {db} from "../db/db";
+
+export const postRoute = Router({})
+
+postRoute.get('/', (req, res) => {
+
+    const blogs = PostRepository.getAllPosts()
+    res.send(blogs)
+})
+
+
+postRoute.get('/:id', (req, res) => {
+    const id = req.params.id
+    const blog = PostRepository.getPostById(id)
+
+    if (!blog) {
+        res.sendStatus(404)
+
+    }
+    res.send(blog)
+})
+
+postRoute.post(
+    '/',
+    authMiddleware,
+    postValidation(),
+    (req: RequestWithBody<CreatePostModel>, res: Response) => {
+
+        let {
+            title,
+            shortDescription,
+            content,
+            blogId
+        } = req.body;
+
+        const newPost = {
+            id: randomUUID(),
+            title,
+            shortDescription,
+            content,
+            blogId,
+            blogName: ""
+        }
+        PostRepository.createPost(newPost);
+        return res.send(201).send(newPost);
+    });
+
+
+postRoute.delete('/:id',
+    authMiddleware,
+
+    (req: RequestWithParams<Params>, res: Response) => {
+        const id = req.params.id;
+        const post = PostRepository.getPostById(id);
+        if (!post) {
+            res.sendStatus(404);
+            return
+        }
+        const postIndex = db.posts.findIndex((p) => p.id == id);
+        if (postIndex == -1) {
+            res.sendStatus(404);
+            return;
+        }
+        db.posts.splice(postIndex, 1);
+        res.sendStatus(204);
+    })
---
+++ src/validators/blog-validator.ts
@@ -0,0 +1,32 @@
+import {body} from "express-validator";
+import {inputValidation} from "../middlewares_validation/input-validation";
+
+export const nameValidation = body('name')
+    .isString().withMessage("Incorrect name")
+    .trim()
+    .isLength({min: 1, max: 15})
+    .withMessage("Incorrect name!");
+
+
+export const descriptionValidation = body('description')
+    .isString()
+    .trim()
+    .isLength({min: 1, max: 500})
+    .withMessage("Incorrect description!");
+
+
+export const websiteUrlValidation = body('websiteUrl')
+    .isString().withMessage("Incorrect name")
+    .trim()
+    .isLength({min: 1, max: 100})
+    .matches('^https://([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$')
+    .withMessage("Incorrect websiteUrl!");
+
+
+export const blogValidation = () =>
+    [
+        nameValidation,
+        descriptionValidation
+        /*websiteUrlValidation,
+        inputValidation*/
+    ]
