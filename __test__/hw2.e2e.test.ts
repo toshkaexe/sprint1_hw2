2aa0f517360698db2048880fce9e17384453bfd8
Anton <anton.zeltser@gmail.com>
2023-12-19 13:41:51 +0100

    bug search

--- __test__/hw2.e2e.test.ts
+++ __test__/hw2.e2e.test.ts
@@ -5,12 +5,22 @@ describe("/testing", () => {
 /*    beforeAll(async () => {
         await request(app).delete("/testing/all-data").expect(204);
     });
-
+*/
     it("Get status 200 and found empty array of blogs, posts", async () => {
-        await request(app).get("/blogs").expect(200, []);
-        await request(app).get("/posts").expect(200, []);
+        await request(app).get("/blogs").expect(200, [{
+            id: '12345',
+            name: 'string',
+            description: 'eins, zwei, drei',
+            websiteUrl: 'www.yandex.ru'
+        },
+            {
+                id: '1',
+                name: 'hello',
+                description: 'vier, fünf, sechs',
+                websiteUrl: 'www.google.com'
+            }]);
+      //  await request(app).get("/posts").expect(200, []);
     })
 
-*/
 
 });
